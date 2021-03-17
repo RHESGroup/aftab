1f99b1c6fada1a97c48fca6d50f2937bce66b8a2
MaryamRP <77168204+MaryamRP@users.noreply.github.com>
2021-03-17 23:08:49 +0330

    Update aftab_core.vhd

--- rtl/aftab_core.vhd
+++ rtl/aftab_core.vhd
@@ -1,121 +1,113 @@
 -- **************************************************************************************
 --	Filename:	aftab_core.vhd
---	Project:	CNL_RISC-V 
---  Version:	1.0
---	History:	
+--	Project:	CNL_RISC-V
+--  	Version:	1.0
+--	History:
 --	Date:		16 February 2021
 --
 -- Copyright (C) 2021 CINI Cybersecurity National Laboratory and University of Teheran
--- 
--- This source file may be used and distributed without 
--- restriction provided that this copyright statement is not 
--- removed from the file and that any derivative work contains 
+--
+-- This source file may be used and distributed without
+-- restriction provided that this copyright statement is not
+-- removed from the file and that any derivative work contains
 -- the original copyright notice and the associated disclaimer.
--- 
--- This source file is free software; you can redistribute it 
--- and/or modify it under the terms of the GNU Lesser General 
+--
+-- This source file is free software; you can redistribute it
+-- and/or modify it under the terms of the GNU Lesser General
 -- Public License as published by the Free Software Foundation;
--- either version 3.0 of the License, or (at your option) any 
--- later version. 
--- 
--- This source is distributed in the hope that it will be 
--- useful, but WITHOUT ANY WARRANTY; without even the implied 
--- warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
--- PURPOSE. See the GNU Lesser General Public License for more 
--- details. 
--- 
--- You should have received a copy of the GNU Lesser General 
--- Public License along with this source; if not, download it 
--- from https://www.gnu.org/licenses/lgpl-3.0.txt 
--- 
+-- either version 3.0 of the License, or (at your option) any
+-- later version.
+--
+-- This source is distributed in the hope that it will be
+-- useful, but WITHOUT ANY WARRANTY; without even the implied
+-- warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+-- PURPOSE. See the GNU Lesser General Public License for more
+-- details.
+--
+-- You should have received a copy of the GNU Lesser General
+-- Public License along with this source; if not, download it
+-- from https://www.gnu.org/licenses/lgpl-3.0.txt
+--
 -- **************************************************************************************
 --
 --	File content description:
 --	Top entity of the AFTAB core
 --
 -- **************************************************************************************
-
 LIBRARY IEEE;
 USE IEEE.std_logic_1164.ALL;
-
 ENTITY aftab_core IS
-	GENERIC( len : INTEGER := 32);
+	GENERIC (len : INTEGER := 32);
 	PORT (
-		clk : IN std_logic;
-		rst : IN std_logic;
-		memReady : IN std_logic;
-		memRead : OUT std_logic;
-		memWrite : OUT std_logic;
-		memDataIN : IN std_logic_vector (7 DOWNTO 0);		
-		memDataOUT : OUT std_logic_vector (7 DOWNTO 0);
-		memAddr : OUT std_logic_vector (len-1 DOWNTO 0)
-		--memAddrDARU : OUT std_logic_vector (len-1 DOWNTO 0)
-		-- : IN std_logic; 
-		--w : OUT std_logic_vector (len-1 DOWNTO 0)
-		);
+		clk        : IN  STD_LOGIC;
+		rst        : IN  STD_LOGIC;
+		memReady   : IN  STD_LOGIC;
+		memRead    : OUT STD_LOGIC;
+		memWrite   : OUT STD_LOGIC;
+		memDataIN  : IN  STD_LOGIC_VECTOR (7 DOWNTO 0);
+		memDataOUT : OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
+		memAddr    : OUT STD_LOGIC_VECTOR (len - 1 DOWNTO 0)
+	);
 END ENTITY;
 --
-ARCHITECTURE procedural OF aftab_core IS 
-	
-	SIGNAL selPCJ : std_logic;
-	SIGNAL selPC : std_logic;
-	SIGNAL selADR : std_logic;
-	SIGNAL selI4 : std_logic;
-	SIGNAL selP2 : std_logic;
-	SIGNAL selP1 : std_logic;
-	SIGNAL selJL : std_logic;
-	SIGNAL selImm : std_logic;
-	SIGNAL selAdd : std_logic;
-	SIGNAL selI4PC : std_logic;
-	SIGNAL selInc4pc : std_logic;
-	SIGNAL selData : std_logic;
-	SIGNAL selBSU : std_logic;
-	SIGNAL selLLU : std_logic;
-	SIGNAL selDARU : std_logic;
-	SIGNAL selASU : std_logic;
-	SIGNAL selAAU : std_logic;
-	SIGNAL shr : std_logic;
-	SIGNAL shl : std_logic;
-	SIGNAL dataInstrBar : std_logic;
-	SIGNAL writeRegFile : std_logic;
-	SIGNAL addSubBar : std_logic;
-	SIGNAL pass : std_logic;
-	SIGNAL selAuipc : std_logic;
-	SIGNAL comparedsignedunsignedbar : std_logic;
-	SIGNAL ldIR : std_logic;
-	SIGNAL ldADR : std_logic;
-	SIGNAL ldPC : std_logic;
-	SIGNAL ldDr : std_logic;
-	SIGNAL setOne : std_logic;
-	SIGNAL setZero : std_logic;
-	SIGNAL startDARU : std_logic;
-	SIGNAL startDAWU : std_logic;
-	SIGNAL completeDARU : std_logic;
-	SIGNAL completeDAWU : std_logic;
-	SIGNAL loadSignedUnsignedBar : std_logic;
-	SIGNAL startMultiplyAAU : std_logic;
-	SIGNAL startDivideAAU : std_logic;
-	SIGNAL completeAAU : std_logic;
-	SIGNAL signedSigned : std_logic;
-	SIGNAL signedUnsigned : std_logic;
-	SIGNAL unsignedUnsigned : std_logic;
-	SIGNAL selAAL : std_logic;
-	SIGNAL selAAH : std_logic;
-	SIGNAL eq : std_logic;
-	SIGNAL gt : std_logic;
-	SIGNAL lt : std_logic;
-	SIGNAL dataerror : std_logic;
-	SIGNAL nBytes : std_logic_vector (1 DOWNTO 0);
-	SIGNAL selLogic : std_logic_vector (1 DOWNTO 0);
-	SIGNAL selShift : std_logic_vector (1 DOWNTO 0);
-	SIGNAL muxCode : std_logic_vector (11 DOWNTO 0);
-	SIGNAL IR : std_logic_vector (31 DOWNTO 0);
-
-
+ARCHITECTURE procedural OF aftab_core IS
+	SIGNAL selPCJ : STD_LOGIC;
+	SIGNAL selPC : STD_LOGIC;
+	SIGNAL selADR : STD_LOGIC;
+	SIGNAL selI4 : STD_LOGIC;
+	SIGNAL selP2 : STD_LOGIC;
+	SIGNAL selP1 : STD_LOGIC;
+	SIGNAL selJL : STD_LOGIC;
+	SIGNAL selImm : STD_LOGIC;
+	SIGNAL selAdd : STD_LOGIC;
+	SIGNAL selI4PC : STD_LOGIC;
+	SIGNAL selInc4pc : STD_LOGIC;
+	SIGNAL selData : STD_LOGIC;
+	SIGNAL selBSU : STD_LOGIC;
+	SIGNAL selLLU : STD_LOGIC;
+	SIGNAL selDARU : STD_LOGIC;
+	SIGNAL selASU : STD_LOGIC;
+	SIGNAL selAAU : STD_LOGIC;
+	SIGNAL shr : STD_LOGIC;
+	SIGNAL shl : STD_LOGIC;
+	SIGNAL dataInstrBar : STD_LOGIC;
+	SIGNAL writeRegFile : STD_LOGIC;
+	SIGNAL addSubBar : STD_LOGIC;
+	SIGNAL pass : STD_LOGIC;
+	SIGNAL selAuipc : STD_LOGIC;
+	SIGNAL comparedsignedunsignedbar : STD_LOGIC;
+	SIGNAL ldIR : STD_LOGIC;
+	SIGNAL ldADR : STD_LOGIC;
+	SIGNAL ldPC : STD_LOGIC;
+	SIGNAL ldDr : STD_LOGIC;
+	SIGNAL ldByteSigned : STD_LOGIC;
+	SIGNAL ldHalfSigned : STD_LOGIC;
+	SIGNAL load : STD_LOGIC;
+	SIGNAL setOne : STD_LOGIC;
+	SIGNAL setZero : STD_LOGIC;
+	SIGNAL startDARU : STD_LOGIC;
+	SIGNAL startDAWU : STD_LOGIC;
+	SIGNAL completeDARU : STD_LOGIC;
+	SIGNAL completeDAWU : STD_LOGIC;
+	SIGNAL startMultiplyAAU : STD_LOGIC;
+	SIGNAL startDivideAAU : STD_LOGIC;
+	SIGNAL completeAAU : STD_LOGIC;
+	SIGNAL signedSigned : STD_LOGIC;
+	SIGNAL signedUnsigned : STD_LOGIC;
+	SIGNAL unsignedUnsigned : STD_LOGIC;
+	SIGNAL selAAL : STD_LOGIC;
+	SIGNAL selAAH : STD_LOGIC;
+	SIGNAL eq : STD_LOGIC;
+	SIGNAL gt : STD_LOGIC;
+	SIGNAL lt  : STD_LOGIC;
+	SIGNAL dataerror : STD_LOGIC;
+	SIGNAL nBytes : STD_LOGIC_VECTOR (1 DOWNTO 0);
+	SIGNAL selLogic : STD_LOGIC_VECTOR (1 DOWNTO 0);
+	SIGNAL selShift : STD_LOGIC_VECTOR (1 DOWNTO 0);
+	SIGNAL muxCode : STD_LOGIC_VECTOR (11 DOWNTO 0);
+	SIGNAL IR : STD_LOGIC_VECTOR (31 DOWNTO 0);
 BEGIN
-
-
-datapath: ENTITY WORK.aftab_datapath PORT MAP(
+	datapathAFTAB : ENTITY WORK.aftab_datapath PORT MAP(
 		clk => clk,
 		rst => rst,
 		writeRegFile => writeRegFile,
@@ -128,17 +120,15 @@ datapath: ENTITY WORK.aftab_datapath PORT MAP(
 		selAdd => selAdd,
 		selP1 => selP1,
 		selADR => selADR,
-		selI4PC => selI4PC,
 		selInc4pc => selInc4pc,
 		selBSU => selBSU,
 		selLLU => selLLU,
 		selASU => selASU,
 		selAAU => selAAU,
 		selDARU => selDARU,
-		selData => selData,
 		selP2 => selP2,
 		selJL => selJL,
-		selImm => selImm ,
+		selImm => selImm,
 		ldPC => ldPC,
 		zeroPC => '0',
 		ldADR => ldADR,
@@ -147,8 +137,10 @@ datapath: ENTITY WORK.aftab_datapath PORT MAP(
 		zeroDR => '0',
 		ldIR => ldIR,
 		zeroIR => '0',
+		ldByteSigned => ldByteSigned,
+		ldHalfSigned => ldHalfSigned,
+		load => load,
 		selShift => selShift,
-
 		addSubBar => addSubBar,
 		pass => pass,
 		selAuipc => selAuipc,
@@ -157,18 +149,14 @@ datapath: ENTITY WORK.aftab_datapath PORT MAP(
 		startDAWU => startDAWU,
 		startDARU => startDARU,
 		memReady => memReady,
-		loadSignedUnsignedBar => loadSignedUnsignedBar,
-		
-		startMultiplyAAU =>  startMultiplyAAU,
-		startDivideAAU => startDivideAAU   , 
-		signedSigned => signedSigned    ,
-		signedUnsigned => signedUnsigned   , 
-		unsignedUnsigned => unsignedUnsigned   , 
-		selAAL => selAAL   ,	
-		selAAH =>  selAAH  , 
-  
-  		completeAAU => completeAAU,
-		
+		startMultiplyAAU => startMultiplyAAU,
+		startDivideAAU => startDivideAAU,
+		signedSigned => signedSigned,
+		signedUnsigned => signedUnsigned,
+		unsignedUnsigned => unsignedUnsigned,
+		selAAL => selAAL,
+		selAAH => selAAH,
+		completeAAU => completeAAU,
 		nBytes => nBytes,
 		memDataIn => memDataIn,
 		memAddrDAWU => memAddr,
@@ -182,13 +170,11 @@ datapath: ENTITY WORK.aftab_datapath PORT MAP(
 		completeDARU => completeDARU,
 		readMem => memRead,
 		writeMem => memWrite,
-		dataError => dataError 
-	);
-
-
-controller: ENTITY WORK.aftab_controller PORT MAP( 
+		dataError => dataError
+		);
+	controllerAFTAB : ENTITY WORK.aftab_controller PORT MAP(
 		clk => clk,
-		rst => rst,  
+		rst => rst,
 		completeDARU => completeDARU,
 		completeDAWU => completeDAWU,
 		completeAAU => completeAAU,
@@ -209,12 +195,12 @@ controller: ENTITY WORK.aftab_controller PORT MAP(
 		selJL => selJL,
 		selImm => selImm,
 		selAdd => selAdd,
-		selInc4PC  => selInc4PC, 
-		selBSU  => selBSU,
-		selLLU  => selLLU, 
-		selASU  => selASU, 
-		selAAU  => selAAU,
-		selDARU  => selDARU, 
+		selInc4PC => selInc4PC,
+		selBSU => selBSU,
+		selLLU => selLLU,
+		selASU => selASU,
+		selAAU => selAAU,
+		selDARU => selDARU,
 		dataInstrBar => dataInstrBar,
 		writeRegFile => writeRegFile,
 		addSubBar => addSubBar,
@@ -225,17 +211,19 @@ controller: ENTITY WORK.aftab_controller PORT MAP(
 		ldADR => ldADR,
 		ldPC => ldPC,
 		ldDr => ldDr,
+		ldByteSigned => ldByteSigned,
+		ldHalfSigned => ldHalfSigned,
+		load => load,
 		setOne => setOne,
 		setZero => setZero,
 		startDARU => startDARU,
 		startDAWU => startDAWU,
-		loadSignedUnsignedBar => loadSignedUnsignedBar,
 		startMultiplyAAU => startMultiplyAAU,
 		startDivideAAU => startDivideAAU,
 		signedSigned => signedSigned,
 		signedUnsigned => signedUnsigned,
 		unsignedUnsigned => unsignedUnsigned,
-		selAAL => selAAL,	
-		selAAH => selAAH 
-	);
+		selAAL => selAAL,
+		selAAH => selAAH
+		);
 END ARCHITECTURE procedural;
