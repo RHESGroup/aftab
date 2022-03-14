75b5a2646b9ec3c5e50018f27279c7400d9527a3
MaryamRP <77168204+MaryamRP@users.noreply.github.com>
2022-03-14 23:50:27 +0330

    Update aftab_divider.vhd

--- rtl/aftab_datapath/aftab_aau/aftab_su_divider/aftab_divider.vhd
+++ rtl/aftab_datapath/aftab_aau/aftab_su_divider/aftab_divider.vhd
@@ -36,79 +36,82 @@
 -- **************************************************************************************
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
-
 ENTITY aftab_divider IS
-	GENERIC (len : INTEGER := 33);
-	PORT (
-		clk          : IN  STD_LOGIC;
-		rst          : IN  STD_LOGIC;
-		startDiv     : IN  STD_LOGIC;
-		doneDiv      : OUT STD_LOGIC;
-		dividend     : IN  STD_LOGIC_VECTOR (len - 1 DOWNTO 0);
-		divisor      : IN  STD_LOGIC_VECTOR (len - 1 DOWNTO 0);
-		Q            : OUT STD_LOGIC_VECTOR (len - 1 DOWNTO 0);
-		Remainder    : OUT STD_LOGIC_VECTOR (len DOWNTO 0));
+	GENERIC
+		(len : INTEGER := 33);
+	PORT
+	(
+		clk       : IN  STD_LOGIC;
+		rst       : IN  STD_LOGIC;
+		startDiv  : IN  STD_LOGIC;
+		doneDiv   : OUT STD_LOGIC;
+		dividend  : IN  STD_LOGIC_VECTOR (len - 1 DOWNTO 0);
+		divisor   : IN  STD_LOGIC_VECTOR (len - 1 DOWNTO 0);
+		Q         : OUT STD_LOGIC_VECTOR (len - 1 DOWNTO 0);
+		Remainder : OUT STD_LOGIC_VECTOR (len DOWNTO 0));
 END ENTITY aftab_divider;
-
 ARCHITECTURE behavioral OF aftab_divider IS
-	SIGNAL R33 			: STD_LOGIC;
-	SIGNAL shRRegR 		: STD_LOGIC;
-	SIGNAL ShLRegR 		: STD_LOGIC;
-	SIGNAL ldRegR 		: STD_LOGIC;
-	SIGNAL zeroRegR 	: STD_LOGIC;
-	SIGNAL QQ0			: STD_LOGIC;
-	SIGNAL seldividend 	: STD_LOGIC;
-	SIGNAL selline1 	: STD_LOGIC;
-	SIGNAL shRRegQ 		: STD_LOGIC;
-	SIGNAL ShLRegQ 		: STD_LOGIC;
-	SIGNAL ldRegQ 		: STD_LOGIC;
-	SIGNAL zeroRegQ		: STD_LOGIC;
-	SIGNAL zeroRegM		: STD_LOGIC;
-	SIGNAL ldRegM 		: STD_LOGIC;
+	SIGNAL R33         : STD_LOGIC;
+	SIGNAL shRRegR     : STD_LOGIC;
+	SIGNAL ShLRegR     : STD_LOGIC;
+	SIGNAL ldRegR      : STD_LOGIC;
+	SIGNAL zeroRegR    : STD_LOGIC;
+	SIGNAL QQ0         : STD_LOGIC;
+	SIGNAL seldividend : STD_LOGIC;
+	SIGNAL selline1    : STD_LOGIC;
+	SIGNAL shRRegQ     : STD_LOGIC;
+	SIGNAL ShLRegQ     : STD_LOGIC;
+	SIGNAL ldRegQ      : STD_LOGIC;
+	SIGNAL zeroRegQ    : STD_LOGIC;
+	SIGNAL zeroRegM    : STD_LOGIC;
+	SIGNAL ldRegM      : STD_LOGIC;
 BEGIN
 	DataPathDiv : ENTITY work.aftab_divider_datapath
-					GENERIC MAP(len => len)
-					PORT MAP(
-						clk => clk, 
-						rst => rst, 
-						dividend => dividend,
-						divisor => divisor, 
-						shRRegR => shRRegR, 
-						ShLRegR => ShLRegR,
-						ldRegR => ldRegR, 
-						zeroRegR => zeroRegR, 
-						QQ0 => QQ0,
-						seldividend => seldividend, 
-						selline1 => selline1, 
-						shRRegQ => shRRegQ, 
-						ShLRegQ => ShLRegQ,
-						ldRegQ => ldRegQ, 
-						zeroRegQ => zeroRegQ, 
-						zeroRegM => zeroRegM,
-						ldRegM => ldRegM, 
-						R33 => R33, 
-						Q => Q, 
-						Remainder => Remainder);
-						
+		GENERIC
+		MAP(len => len)
+		PORT MAP
+		(
+		clk         => clk,
+		rst         => rst,
+		dividend    => dividend,
+		divisor     => divisor,
+		shRRegR     => shRRegR,
+		ShLRegR     => ShLRegR,
+		ldRegR      => ldRegR,
+		zeroRegR    => zeroRegR,
+		QQ0         => QQ0,
+		seldividend => seldividend,
+		selline1    => selline1,
+		shRRegQ     => shRRegQ,
+		ShLRegQ     => ShLRegQ,
+		ldRegQ      => ldRegQ,
+		zeroRegQ    => zeroRegQ,
+		zeroRegM    => zeroRegM,
+		ldRegM      => ldRegM,
+		R33         => R33,
+		Q           => Q,
+		Remainder   => Remainder);
 	ControllerDiv : ENTITY work.aftab_divider_controller
-						GENERIC MAP(len => len)
-						PORT MAP(
-							clk => clk, 
-							rst => rst, 
-							startDiv => startDiv,
-							R33 => R33, 
-							doneDiv => doneDiv, 
-							shRRegR => shRRegR,
-							ShLRegR => ShLRegR, 
-							ldRegR => ldRegR, 
-							zeroRegR => zeroRegR,
-							seldividend => seldividend, 
-							selline1 => selline1, 
-							shRRegQ => shRRegQ, 
-							ShLRegQ => ShLRegQ,
-							ldRegQ => ldRegQ, 
-							zeroRegQ => zeroRegQ, 
-							zeroRegM => zeroRegM,
-							ldRegM => ldRegM, 
-							QQ0 => QQ0);
+		GENERIC
+		MAP(len => len)
+		PORT
+		MAP(
+		clk         => clk,
+		rst         => rst,
+		startDiv    => startDiv,
+		R33         => R33,
+		doneDiv     => doneDiv,
+		shRRegR     => shRRegR,
+		ShLRegR     => ShLRegR,
+		ldRegR      => ldRegR,
+		zeroRegR    => zeroRegR,
+		seldividend => seldividend,
+		selline1    => selline1,
+		shRRegQ     => shRRegQ,
+		ShLRegQ     => ShLRegQ,
+		ldRegQ      => ldRegQ,
+		zeroRegQ    => zeroRegQ,
+		zeroRegM    => zeroRegM,
+		ldRegM      => ldRegM,
+		QQ0         => QQ0);
 END ARCHITECTURE behavioral;
