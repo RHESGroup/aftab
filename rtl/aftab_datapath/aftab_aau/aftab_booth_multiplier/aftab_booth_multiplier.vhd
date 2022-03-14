837a5cd492e4cc1890eab9c27543b84bfd9ba792
MaryamRP <77168204+MaryamRP@users.noreply.github.com>
2022-03-14 23:48:16 +0330

    Update aftab_booth_multiplier.vhd

--- rtl/aftab_datapath/aftab_aau/aftab_booth_multiplier/aftab_booth_multiplier.vhd
+++ rtl/aftab_datapath/aftab_aau/aftab_booth_multiplier/aftab_booth_multiplier.vhd
@@ -36,60 +36,63 @@
 -- **************************************************************************************
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
-
 ENTITY aftab_booth_multiplier IS
-	GENERIC (len : INTEGER := 33);
-	PORT (
-		clk 	   : IN  STD_LOGIC;
-		rst        : IN  STD_LOGIC;
-		startBooth : IN  STD_LOGIC;
-		M          : IN  STD_LOGIC_VECTOR (len - 1 DOWNTO 0);
-		Q          : IN  STD_LOGIC_VECTOR (len - 1 DOWNTO 0);
-		P          : OUT STD_LOGIC_VECTOR (2 * len - 1 DOWNTO 0);
-		doneBooth  : OUT STD_LOGIC
+	GENERIC
+		(len : INTEGER := 33);
+	PORT
+	(
+	   clk        : IN  STD_LOGIC;
+	   rst        : IN  STD_LOGIC;
+	   startBooth : IN  STD_LOGIC;
+	   M          : IN  STD_LOGIC_VECTOR (len - 1 DOWNTO 0);
+	   Q          : IN  STD_LOGIC_VECTOR (len - 1 DOWNTO 0);
+	   P          : OUT STD_LOGIC_VECTOR (2 * len - 1 DOWNTO 0);
+	   doneBooth  : OUT STD_LOGIC
 	);
 END ENTITY aftab_booth_multiplier;
-
 ARCHITECTURE behavioral OF aftab_booth_multiplier IS
-	SIGNAL op   	: STD_LOGIC_VECTOR (1 DOWNTO 0);
-	SIGNAL shrQ 	: STD_LOGIC;
-	SIGNAL ldQ 		: STD_LOGIC;
-	SIGNAL ldM 		: STD_LOGIC;
-	SIGNAL ldP 		: STD_LOGIC;
-	SIGNAL zeroP 	: STD_LOGIC;
-	SIGNAL sel 		: STD_LOGIC;
-	SIGNAL subSel 	: STD_LOGIC;
+	SIGNAL op     : STD_LOGIC_VECTOR (1 DOWNTO 0);
+	SIGNAL shrQ   : STD_LOGIC;
+	SIGNAL ldQ    : STD_LOGIC;
+	SIGNAL ldM    : STD_LOGIC;
+	SIGNAL ldP    : STD_LOGIC;
+	SIGNAL zeroP  : STD_LOGIC;
+	SIGNAL sel    : STD_LOGIC;
+	SIGNAL subSel : STD_LOGIC;
 BEGIN
-	Datapath :	 ENTITY WORK.aftab_booth_multiplier_datapath
-					GENERIC MAP(len => len)
-					PORT MAP(
-						clk => clk, 
-						rst => rst, 
-						shrQ => shrQ, 
-						ldQ => ldQ, 
-						ldM => ldM,
-						ldp => ldp, 
-						zeroP => zeroP, 
-						sel => sel,
-						subsel => subsel, 
-						M => M, 
-						Q => Q, 
-						P => P, 
-						op => op);
-						
+	Datapath : ENTITY WORK.aftab_booth_multiplier_datapath
+		GENERIC
+		MAP(len => len)
+		PORT MAP
+		(
+		 clk    => clk,
+		 rst    => rst,
+		 shrQ   => shrQ,
+		 ldQ    => ldQ,
+		 ldM    => ldM,
+		 ldp    => ldp,
+		 zeroP  => zeroP,
+		 sel    => sel,
+		 subsel => subsel,
+		 M      => M,
+		 Q      => Q,
+		 P      => P,
+		 op     => op);
 	Controller : ENTITY WORK.aftab_booth_multiplier_controller
-					GENERIC MAP(len => len)
-					PORT MAP(
-						clk => clk, 
-						rst => rst,
-						startBooth => startBooth,
-						shrQ => shrQ, 
-						ldQ => ldQ, 
-						ldM => ldM,
-						ldP => ldP, 
-						zeroP => zeroP, 
-						sel => sel,
-						subSel => subSel,
-						op => op, 
-						done => doneBooth);
+		GENERIC
+		MAP(len => len)
+		PORT
+		MAP(
+		clk        => clk,
+		rst        => rst,
+		startBooth => startBooth,
+		shrQ       => shrQ,
+		ldQ        => ldQ,
+		ldM        => ldM,
+		ldP        => ldP,
+		zeroP      => zeroP,
+		sel        => sel,
+		subSel     => subSel,
+		op         => op,
+		done       => doneBooth);
 END ARCHITECTURE behavioral;
