15ba3995472854dac65be960acf73369220f52bc
Zahra-jahanpeyma <77167575+Zahra-jahanpeyma@users.noreply.github.com>
2022-03-21 12:40:07 +0330

    Update aftab_multiplexer.vhd

--- rtl/aftab_datapath/aftab_multiplexer.vhd
+++ rtl/aftab_datapath/aftab_multiplexer.vhd
@@ -5,7 +5,7 @@
 --	History:	
 --	Date:		16 February 2021
 --
--- Copyright (C) 2021 CINI Cybersecurity National Laboratory and University of Teheran
+-- Copyright (C) 2021 CINI Cybersecurity National Laboratory and University of Tehran
 -- 
 -- This source file may be used and distributed without 
 -- restriction provided that this copyright statement is not 
@@ -48,7 +48,7 @@ ENTITY aftab_multiplexer IS
 		w    : OUT STD_LOGIC_VECTOR (len-1 DOWNTO 0)
 	);
 END ENTITY aftab_multiplexer;
-
+--
 ARCHITECTURE procedural OF aftab_multiplexer IS BEGIN
 	PROCESS (a, b, s0, s1) BEGIN
 		IF (s0 = '1') THEN 
