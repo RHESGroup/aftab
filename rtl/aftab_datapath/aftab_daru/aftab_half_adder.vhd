bc96cf7d19fd10302cb7dde082f2de7c41f29093
MaryamRP <77168204+MaryamRP@users.noreply.github.com>
2022-03-25 15:34:02 +0430

    Update aftab_half_adder.vhd

--- rtl/aftab_datapath/aftab_daru/aftab_half_adder.vhd
+++ rtl/aftab_datapath/aftab_daru/aftab_half_adder.vhd
@@ -1,3 +1,39 @@
+-- **************************************************************************************
+--	Filename:	aftab_oneBitReg.vhd
+--	Project:	CNL_RISC-V
+--  Version:	1.0
+--	History:
+--	Date:		16 February 2021
+--
+-- Copyright (C) 2021 CINI Cybersecurity National Laboratory and University of Tehran
+--
+-- This source file may be used and distributed without
+-- restriction provided that this copyright statement is not
+-- removed from the file and that any derivative work contains
+-- the original copyright notice and the associated disclaimer.
+--
+-- This source file is free software; you can redistribute it
+-- and/or modify it under the terms of the GNU Lesser General
+-- Public License as published by the Free Software Foundation;
+-- either version 3.0 of the License, or (at your option) any
+-- later version.
+--
+-- This source is distributed in the hope that it will be
+-- useful, but WITHOUT ANY WARRANTY; without even the implied
+-- warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+-- PURPOSE. See the GNU Lesser General Public License for more
+-- details.
+--
+-- You should have received a copy of the GNU Lesser General
+-- Public License along with this source; if not, download it
+-- from https://www.gnu.org/licenses/lgpl-3.0.txt
+--
+-- **************************************************************************************
+--
+--	File content description:
+--	one bit register for the AFTAB core
+--
+-- **************************************************************************************
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.STD_LOGIC_UNSIGNED.ALL;
